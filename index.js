a752cdcbea9ad1cb394cdeff0dc8f40bd83e8786
scorpionmars <bjean62@gmail.com>
2022-05-08 10:53:27 -0400

    5/8/22

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
+localStorage.setItem("myLeads", )
+
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
@@ -13,7 +15,13 @@ function renderLeads() {
     let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
   
-        listItems += `<li><a target='_blank' href='  ${myLeads[i]}  "'> ${myLeads[i]}  </a></li>`
+        listItems += `
+        <li>
+            <a target='_blank' href='  ${myLeads[i]} '> 
+          ${myLeads[i]}  
+            </a>
+        </li>`
     }
     ulEl.innerHTML = listItems  
 }
+
